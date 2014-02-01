fc9464d2e3c8986b09f4f7b690a8a61e2afcda0a
prohorme <prokhor_shlyapkin@mail.ru>
2014-02-01 14:55:30 +0600

    Create sasta.js

---
+++ sasta.js
@@ -0,0 +1,52 @@
+var iteration = 0;  
+var limitation= 0;
+var fb = undefined; 
+
+
+var sas = function() {
+  if(window.location.href != $('#myprofile')[0].href) { 
+   fb.hide(); 
+    showFastBox('Error','Go to your profile page and try again.'); 
+    return; 
+  }
+  if(iteration == limitation) { 
+    fb.hide(); 
+    showFastBox('VK HidePhoto', 'Photos successfully hidden! :)'); 
+    return; 
+  }
+  try { 
+    $('.profile_photo_hide')[0].click(); 
+  } catch(e) {
+    console.log('DEPH ERROR '+e); 
+    fb.hide(); 
+    showFastBox('VK HidePhoto Error','Error in the photo...'+iteration.toString()+'<br>'+e.toString()); 
+    return; 
+  } 
+  iteration = iteration + 1; 
+  setTimeout(sas, 500); 
+}
+
+
+var dephotify = function() {
+  var input = $('#depho_amt').val(); 
+    firstBox.hide(); 
+  if(isNaN(parseInt(input))) {  
+   showFastBox('Invalid input!', 'Type the numbers!');
+   return;
+  }
+  limitation = parseInt(input); /
+  sas(); 
+  fb = showFastBox('VK HidePhoto','Goes hide images wait ...'); 
+  fb.removeButtons(); 
+  fb.showProgress(); 
+  $('.box_x_button')[0].style.display="none"; 
+}
+var firstBox = undefined;
+
+
+var main = function() {
+  firstBox = showFastBox('VK HidePhoto','How many photos can be removed from the panel?','Hide', function() { dephotify(); }, 'Cancel');
+  firstBox.bodyNode.innerHTML =  firstBox.bodyNode.innerHTML + "<br><input type=\"text\" id=\"depho_amt\" placeholder=\"Nubmer, example 5\" value=\"5\">";
+}
+
+main();
